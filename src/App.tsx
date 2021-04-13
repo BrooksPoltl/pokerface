46e2ac2d687e2018d3054bc8ded5fbbfd3555308
Brooks Poltl <bpoltl1@gmail.com>
2021-04-13 08:01:35 -0500

    adding header and setting token

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axiosInstance from './axiosInstance'
 
+type User = {
+  id: number
+  name: string
+  cash: number
+}
 const App = () => {
   const [name, setName] = useState("");
+  const [user, setUser ] = useState<User | null>(null);
+
   useEffect(() => {
-    fetchUser();
+    axiosInstance.interceptors.request.use(function (config) {
+      const token = localStorage.getItem('token');
+      config.headers.Authorization =  token ? `Bearer ${token}` : '';
+      return config;
+    });
+    
   },[])
 
-  const fetchUser = async() => {
-    await axios.get("http://localhost:4000/api")
+  const fetchUser = async(cred = false) => {
+    await axiosInstance.get("/")
   }
 
   const createUser = async(event: React.FormEvent) => {
     event.preventDefault();
-    const user = await axios.post("http://localhost:4000/api", { name })
-    
+    const { data: {user, token }} = await axiosInstance.post("/", { name })
+    console.log(user)
+    setUser(user)
+    localStorage.setItem('token', token);
     setName("");
   }
 
-  return (  
-  <div className="border border-red h-screen">  
-  <div className="left-2/4 top-2/4 fixed absolute transform -translate-y-1/2 -translate-x-1/2 border border-black w-3/12">
+   return  !user? (
+      <div className="border border-red h-screen">  
+  <div className="left-2/4 top-2/4 fixed transform -translate-y-1/2 -translate-x-1/2 border border-black w-3/12">
     <form className = "flex flex-col text-center items-center" onSubmit={createUser}>
       <span>Create a user to play!</span>
       <label>
@@ -31,7 +45,10 @@ const App = () => {
     </form>
   </div>
   </div>
-  );
+    ):
+    <div>
+      {user!.id}
+    </div> 
 }
 
 export default App;
