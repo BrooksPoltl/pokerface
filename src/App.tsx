5cea43a06ec4b9b071a61e2c41f7b70fb0155634
Brooks Poltl <bpoltl1@gmail.com>
2021-04-14 06:54:14 -0500

    linter is configured for real now

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import axiosInstance from './axiosInstance'
+import { AxiosRequestConfig } from 'axios';
+import axiosInstance from './axiosInstance';
 
 type User = {
   id: number
@@ -7,48 +8,57 @@ type User = {
   cash: number
 }
 const App = () => {
-  const [name, setName] = useState("");
-  const [user, setUser ] = useState<User | null>(null);
+  const [name, setName] = useState('');
+  const [userData, setUserData] = useState<User | null>(null);
+  const fetchUser = async () => {
+    await axiosInstance.get('/');
+  };
 
   useEffect(() => {
-    axiosInstance.interceptors.request.use(function (config) {
+    axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
       const token = localStorage.getItem('token');
-      config.headers.Authorization =  token ? `Bearer ${token}` : '';
-      return config;
+      const conf = config;
+      conf.headers.Authorization = token ? `Bearer ${token}` : '';
+      return conf;
     });
-    
-  },[])
+    fetchUser();
+  }, []);
 
-  const fetchUser = async(cred = false) => {
-    await axiosInstance.get("/")
-  }
-
-  const createUser = async(event: React.FormEvent) => {
+  const createUser = async (event: React.FormEvent) => {
     event.preventDefault();
-    const { data: {user, token }} = await axiosInstance.post("/", { name })
-    console.log(user)
-    setUser(user)
+    const { data: { user, token } } = await axiosInstance.post('/', { name });
+    console.log(user);
+    setUserData(user);
     localStorage.setItem('token', token);
-    setName("");
-  }
+    setName('');
+  };
 
-   return  !user? (
-      <div className="border border-red h-screen">  
-  <div className="left-2/4 top-2/4 fixed transform -translate-y-1/2 -translate-x-1/2 border border-black w-3/12">
-    <form className = "flex flex-col text-center items-center" onSubmit={createUser}>
-      <span>Create a user to play!</span>
-      <label>
-        Name: 
-      <input className="rounded border border-black" type="text" value={name} onChange={e => setName(e.target.value)}></input>
-      </label>
-    <button className="m-2 w-3/12 border-2 border-purple-500 hover:border-gray-500">submit</button>
-    </form>
-  </div>
-  </div>
-    ):
-    <div>
-      {user!.id}
-    </div> 
-}
+  return !userData ? (
+    <div className="border border-red h-screen">
+      <div className="left-2/4 top-2/4 fixed transform
+      -translate-y-1/2 -translate-x-1/2 border border-black w-3/12"
+      >
+        <form className="flex flex-col text-center items-center" onSubmit={createUser}>
+          <span>Create a user to play!</span>
+          <label htmlFor="name">
+            <span>Name: </span>
+            <input name="name" className="rounded border border-black" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <button
+            type="submit"
+            className="m-2 w-3/12 border-2 border-purple-500 hover:border-gray-500"
+          >
+            submit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+    : (
+      <div>
+        {userData!.id}
+      </div>
+    );
+};
 
 export default App;
--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
- 
-const axiosInstance = axios.create({ baseURL: "http://localhost:4000/api"})
+const axiosInstance = axios.create({ baseURL: 'http://localhost:4000/api' });
 
 export default axiosInstance;
