74c99d7a29c86fc78d819df52b6fee9b51caecbb
Brooks Poltl <bpoltl1@gmail.com>
2021-04-16 15:05:28 -0500

    creating rooms

--- src/components/ShowRooms.tsx
+++ src/components/ShowRooms.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosInstance from '../axiosInstance';
 import Room from './Room';
 
 const ShowRooms = () => {
-  const [rooms, setRooms] = useState([]);
+  const [roomsData, setRoomsData] = useState([]);
+  const [name, setName] = useState('');
 
-  const createRoom = (e) => {
+  useEffect(() => {
+    listRooms();
+  }, []);
+
+  const listRooms = async () => {
+    const { data: rooms } = await axiosInstance.get('/rooms');
+    setRoomsData(rooms);
+  };
+
+  const createRoom = async (e) => {
     e.preventDefault();
-    setRooms([]);
+    await axiosInstance.post('/rooms', { room: { name } });
+    setName('');
+    listRooms();
   };
   return (
     <div>
-      aadad
-      { rooms.map((room) => <Room room={room} />)}
-      <button type="button" onClick={createRoom}>create room</button>
+      { roomsData.map((r) => <Room room={r} />)}
+      <form className="flex flex-col text-center items-center">
+        <p>Please create a room</p>
+        <label htmlFor="name">
+          <span>Name: </span>
+          <input name="name" className="rounded border border-black" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <button className="m-2 w-3/12 border-2 border-purple-500 hover:border-gray-500" type="button" onClick={createRoom}>create room</button>
+      </form>
     </div>
   );
 };
