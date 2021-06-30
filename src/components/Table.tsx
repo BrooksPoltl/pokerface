d14e86480dfaf70adf72e20540950b221adbf914
Brooks Poltl <bpoltl1@gmail.com>
2021-06-30 06:04:04 -0500

    setup table and unicaode

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -74,12 +74,15 @@ const Table = () => {
         setHands(hands);
       });
       chan.on('new_event', ({
-        type, amt, user_id, turn,
+        type, amt, user_id, turn, game,
       }) => {
         const hand = turn.hands.find((h) => h.user_id === user_id);
         setEvents((oldEvents) => [...oldEvents, {
           type, amt, user_id, name: hand.name,
         }]);
+        if (game) {
+          setGame(game);
+        }
         setHands(turn.hands);
         setActivePlayer(turn.user_id);
       });
@@ -91,68 +94,102 @@ const Table = () => {
       setChannel(chan);
     }
   };
+  // eslint-disable-next-line consistent-return
+  function getUnicodeCharacter(cp) {
+    // eslint-disable-next-line no-mixed-operators
+    if (cp >= 0 && cp <= 0xD7FF || cp >= 0xE000 && cp <= 0xFFFF) {
+      return String.fromCharCode(cp);
+    } if (cp >= 0x10000 && cp <= 0x10FFFF) {
+      // eslint-disable-next-line no-param-reassign
+      cp -= 0x10000;
+      // eslint-disable-next-line no-bitwise
+      const first = ((0xffc00 & cp) >> 10) + 0xD800;
+      // eslint-disable-next-line no-bitwise
+      const second = (0x3ff & cp) + 0xDC00;
 
+      return String.fromCharCode(first) + String.fromCharCode(second);
+    }
+  }
+
+  const generatePosition = (i) => {
+    if (i === 0) {
+      return 'top-3/4 left-1/4 transform -translate-y-1/2';
+    }
+    if (i === 1) {
+      return 'top-2/4 left-1/4 transform -translate-y-1/2';
+    }
+    if (i === 2) {
+      return 'top-1/4 left-1/4 transform -translate-y-1/2';
+    }
+    if (i === 3) {
+      return 'top-1/4 left-2/4 transform -translate-y-1/2 -translate-x-1/2';
+    }
+    if (i === 4) {
+      return 'top-1/4 left-3/4 transform -translate-y-1/2 -translate-x-full';
+    }
+    if (i === 5) {
+      return 'top-2/4 left-3/4 transform -translate-y-1/2 -translate-x-full';
+    }
+    if (i === 6) {
+      return 'top-3/4 left-3/4 transform -translate-y-1/2 -translate-x-full';
+    }
+    if (i === 7) {
+      return 'top-3/4 left-2/4 transform -translate-y-1/2 -translate-x-1/2';
+    }
+    return '';
+  };
+
+  const transformCardToUnicode = (card) => {
+    if (card === '??') {
+      return getUnicodeCharacter(0x1F0A0);
+    }
+    const suitObj = {
+      S: 'A',
+      H: 'B',
+      D: 'C',
+      C: 'D',
+    };
+    const numObj = {
+      A: 1,
+      10: 'A',
+      J: 'B',
+      Q: 'D',
+      K: 'E',
+    };
+    const base = `0x1F0${suitObj[card[0]]}${numObj[card.substring(1, card.length)] ? numObj[card.substring(1, card.length)] : card.substring(1, card.length)}`;
+
+    return getUnicodeCharacter(base);
+  };
   useEffect(() => {
     joinRoom();
   }, []);
   return (
-    <div>
-      <p>Welcome to the table!</p>
-      { hands.map((h) => (
-        <div className="flex m-3" key={h.user_id}>
-          <div className="mx-3">
-            {' '}
-            {h.name}
-            {' '}
-            {h.cash}
-            {h.is_big_user && <p>B</p>}
-            {h.is_small_user && <p>S</p>}
-          </div>
-          {activePlayer === h.user_id && <p>Active</p>}
-          card1:
-          {' '}
-          <p className="mx-3">{h.card1 ? h.card1 : '??'}</p>
-          card2:
-          {' '}
-          <p className="mx-3">{h.card2 ? h.card2 : '??'}</p>
-        </div>
-      ))}
-      <div>
+    <div className="relative border-4 h-screen">
+      <div className="absolute left-2/4 bottom-2/4 transform translate-y-1/2 -translate-x-1/2">
         <div>
-          cards:
-          {' '}
-          <p className="mx-3">
-            card1:
+          <div className="flex">
+            <p className={`mx-3 text-8xl ${!game.card1 || (game.card1 && ['H', 'D'].includes(game.card1[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+              {game.card1 ? transformCardToUnicode(game.card1) : transformCardToUnicode('??') }
+            </p>
             {' '}
-            {game.card1 ? game.card1 : '??' }
-          </p>
-          {' '}
-          <p className="mx-3">
-            card2:
+            <p className={`mx-3 text-8xl ${!game.card2 || (game.card2 && ['H', 'D'].includes(game.card2[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+              {game.card2 ? transformCardToUnicode(game.card2) : transformCardToUnicode('??') }
+            </p>
             {' '}
-            {game.card2 ? game.card2 : '??' }
-          </p>
-          {' '}
-          <p className="mx-3">
-            card3:
+            <p className={`mx-3 text-8xl ${!game.card3 || (game.card3 && ['H', 'D'].includes(game.card3[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+              {game.card3 ? transformCardToUnicode(game.card3) : transformCardToUnicode('??') }
+            </p>
             {' '}
-            {game.card3 ? game.card3 : '??' }
-          </p>
-          {' '}
-          <p className="mx-3">
-            card4:
+            <p className={`mx-3 text-8xl ${!game.card4 || (game.card4 && ['H', 'D'].includes(game.card4[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+              {game.card4 ? transformCardToUnicode(game.card4) : transformCardToUnicode('??') }
+            </p>
             {' '}
-            {game.card4 ? game.card4 : '??' }
-          </p>
-          {' '}
-          <p className="mx-3">
-            card5:
-            {' '}
-            {game.card5 ? game.card5 : '??' }
-          </p>
-
+            <p className={`mx-3 text-8xl ${!game.card5 || (game.card5 && ['H', 'D'].includes(game.card5[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+              {game.card5 ? transformCardToUnicode(game.card5) : transformCardToUnicode('??') }
+            </p>
+          </div>
         </div>
-        <p>
+        {/* <p>
           BIG USER ID
           {' '}
           {game.big_user_id}
@@ -161,7 +198,29 @@ const Table = () => {
           SMALL USER ID
           {' '}
           {game.small_user_id}
-        </p>
+        </p> */}
+      </div>
+      <div className="">
+        { hands.map((h, i) => (
+          <div className={`absolute flex flex-col m-3 ${generatePosition(i)}`} key={h.user_id}>
+            <div className="flex">
+              <p className={`mx-3 text-8xl ${!h.card1 || (h.card1 && ['H', 'D'].includes(h.card1[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+                {h.card1 ? transformCardToUnicode(h.card1) : transformCardToUnicode('??')}
+              </p>
+              <p className={`mx-3 text-8xl ${!h.card2 || (h.card2 && ['H', 'D'].includes(h.card2[0])) ? 'text-red-600' : 'text-gray-800'}`}>
+                {h.card2 ? transformCardToUnicode(h.card2) : transformCardToUnicode('??')}
+              </p>
+            </div>
+            <div className="mx-3">
+              <p className={activePlayer !== h.user_id ? 'text-gray-200' : 'text-gray-900'}>
+                {' '}
+                {h.name}
+                {' '}
+                {h.cash}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
       <SelectionButtons
         gameId={game.id}
